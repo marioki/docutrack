d4416eb20ff822b707e4b0766640b8665259f092
Mario Kirven <mariokirven.mk@gmail.com>
2025-07-20 20:47:50 -0500

    Update supabase.ts

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL!;
 const anonKey = process.env.SUPABASE_ANON_KEY!;
-const serviceKey = process.env.SUPABASE_SERVICE_ROLE!;
+const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 export const supabase = createClient(supabaseUrl, anonKey, {
     auth: { persistSession: false },
